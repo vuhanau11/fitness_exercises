91137d5168d1771f4978f1137379234e45f642b2
tungnv <tungnvt1610@gmail.com>
2022-09-03 22:15:30 +0700

    add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Banner from '../components/Banner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <Box>
+      <Banner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
 }
 
 export default Home
